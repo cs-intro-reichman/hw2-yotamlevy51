bdb68f1d40567e35424d40d33b401be3b24b9b0b
yotam levy <yotam.levy01@post.runi.ac.il>
2024-11-16 17:00:38 +0200

    done

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,14 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int N = Integer.parseInt(args [0]);
+	    double p = 0;
+	 
+	 
+	     for (int i = 0; i < N; i ++) {
+	     	p = p + Math.pow(-1, i)/ ((2 * i) + 1);
+	     }
+	    System.out.println("pi according to Java: " + Math.PI);
+	    System.out.println("pi, approximated:     " + (p*4));
 	}
 }
